63a7d6a83409a54243255d0b81b448ccdb77bd19
Pedro Paiva <pedrovsp109@gmail.com>
2018-08-25 00:27:07 -0300

    sessao: componentes interativos com state

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 import HelloWorld from './HelloWorld';// nosso primeiro componente React!
+import ContaClick from './ContaClick';
 
 class App extends Component {
   render() {
     const nomes = ["Lucas", "Tableless", "Leitor", "Maria", "João", "Ana"];
     return <div>
       {nomes.map((n, i) => <HelloWorld nome={n} key={i}/>)}
+      <ContaClick />
     </div>;
   }
 }
